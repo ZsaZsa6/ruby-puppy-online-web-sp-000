c9afd50c1752c352e53d70c8164bee9bc2a064fb
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-18 13:46:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# Add your code here
+# require "byebug"
+class Dog
+
+  @@all = []
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  # def self.save
+  #   @@all << self
+  # end
+# byebug
+  def self.all
+    @@all
+  end
+
+ def self.print_all
+   @@all.map do
+   puts "#{name}"
+   end
+ end
+
+end
